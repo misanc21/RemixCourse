efd811bdc3bf75b76363c86eae9bbfe9e9edd081
misanc21 <misanc21@gmail.com>
2023-06-28 11:40:08 -0600

    loader example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,19 @@
+import  { json, type LoaderArgs} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+//Loader se ejecuta en el server
+export function loader(_:LoaderArgs) {
+  const fakeData = {
+    msg:'Que onda que onda',
+    status:200
+  }
+
+  return json({...fakeData});
+}
+
+
 export default function App() {
-  return <h1 className="underline">hola a todos!</h1>
+  const {msg} = useLoaderData<typeof loader>();
+  
+  return <h1 className="text-5xl text-center text-orange-200">{msg}</h1>
 }
